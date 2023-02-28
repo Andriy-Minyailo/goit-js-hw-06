f76360645e831e4a15492806b57644951cf0c978
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-28 20:47:42 +0200

    add_task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,42 @@
+const formLogin = document.querySelector('.login-form');
 
+formLogin.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (!email.value || !password.value) {
+        alert('Please fill in all fields');
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(formData);
+    }
+event.currentTarget.reset();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onFormSubmit (event) {
+//     event.preventDefault();
+//     const formData = new FormData(event.currentTarget);
+//     console.log(formData);
+//     formData.forEach((value, name) => {
+//         console.log(name);
+//         console.log(value);
+//     })
+// }
