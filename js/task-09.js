4fb3fa784ee8d60536baf03446be4a70d6dbb39b
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-28 21:50:13 +0200

    add_task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const buttonChangeColor = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', onColorSubmit);
+
+function onColorSubmit(event) {
+  event.preventDefault();
+  const colorBody = getRandomHexColor();
+  body.style.backgroundColor = colorBody;
+  spanColor.textContent = `${colorBody}`;
+ }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
