849972aed285ee090eb60bee6038c5c47d5e9029
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-03-04 12:26:23 +0200

    fixed task2

--- js/task-02.js
+++ js/task-02.js
@@ -9,14 +9,25 @@ const ingredients = [
 const ulIngredients = document.querySelector('#ingredients');
 
 function addIngredientsLi (array) {
-  const allLi = [];
-  array.forEach(elem => {
+  const allLi = array.map(elem => {
     const ingredientLi = document.createElement("li");
     ingredientLi.textContent = elem;
     ingredientLi.classList.add("item");
-    allLi.push(ingredientLi);
+    return ingredientLi;
     })
   ulIngredients.append(...allLi);
 }
 
 addIngredientsLi(ingredients);
+
+// function addIngredientsLi (array) {
+//   const allLi = [];
+//   array.forEach(elem => {
+//     const ingredientLi = document.createElement("li");
+//     ingredientLi.textContent = elem;
+//     ingredientLi.classList.add("item");
+//     allLi.push(ingredientLi);
+//     })
+//   ulIngredients.append(...allLi);
+// }
+
